fb382b2555df166acf5d898495513b49d4ad762b
Tarun Ramasamy Tamilselvan <tr519@student.aru.ac.uk>
2024-11-26 11:45:28 +0000

    renamed form1 as login page

--- WindowsForms/loginpage.Designer.cs
+++ WindowsForms/loginpage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsForms
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
