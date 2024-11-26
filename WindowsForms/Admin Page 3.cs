93c4a20f26524fb2557674937f0dd6ee17815d9b
hds3131 <hetanshusevak2021@gmail.com>
2024-11-26 14:04:26 +0000

    Revert "Merge branch 'master' of https://github.com/hds3131/WindowsForms"
    This reverts commit 4039649ab4e0191bd3cf9e4f8efe92a8f0d39ba3, reversing
    changes made to ef0af997be73ceebfaa53f29c6537823e521862a.

--- WindowsForms/Admin Page 3.cs
+++ WindowsForms/Admin Page 3.cs
@@ -10,19 +10,24 @@ using System.Windows.Forms;
 
 namespace WindowsForms
 {
-    public partial class Form10 : Form
+    public partial class Form7 : Form
     {
-        public Form10()
+        public Form7()
         {
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
 
-        private void textBox8_TextChanged(object sender, EventArgs e)
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
