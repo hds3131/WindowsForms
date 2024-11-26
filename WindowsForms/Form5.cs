cd3d653e86e835fb7ac730e8630f2e0338adb7de
hds3131 <hetanshusevak2021@gmail.com>
2024-11-26 13:26:07 +0000

    Revert "Redesigned"
    This reverts commit cc431654b32d42ecdf980fb021f67186dd6aad0f.

--- WindowsForms/Form5.cs
+++ WindowsForms/Form5.cs
@@ -86,15 +86,5 @@ namespace WindowsForms
         {
 
         }
-
-        private void dataGridView12_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void linkLabel11_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-
-        }
     }
 }
