5d5cc4f66659d19c7796b000b28030a3341a8e32
amygeer12 <ag1510@student.aru.ac.uk>
2024-11-28 18:08:18 +0000

    Buttons

--- WindowsForms/Form8.cs
+++ WindowsForms/Form8.cs
@@ -41,5 +41,15 @@ namespace WindowsForms
         {
 
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
