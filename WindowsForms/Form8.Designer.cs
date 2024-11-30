27e061adfb11c0b1fb52f7cb10a90976453d88e2
hds3131 <hetanshusevak2021@gmail.com>
2024-11-30 18:47:18 +0000

    Update Form8.Designer.cs

--- WindowsForms/Form8.Designer.cs
+++ WindowsForms/Form8.Designer.cs
@@ -282,7 +282,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(217, 419);
+            this.button4.Location = new System.Drawing.Point(241, 418);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 48;
