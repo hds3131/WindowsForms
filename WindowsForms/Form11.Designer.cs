97d207df3a78761492ac442b32d30a9caeb7aa05
Tarun Ramasamy Tamilselvan <tr519@student.aru.ac.uk>
2024-11-30 19:30:36 +0000

    Revert "Update Form11.Designer.cs"
    This reverts commit da54ffb7259fbd13c608119561e850f31f6aabbe.

--- WindowsForms/Form11.Designer.cs
+++ WindowsForms/Form11.Designer.cs
@@ -216,7 +216,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(300, 108);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(110, 22);
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 41;
             // 
             // textBox3
