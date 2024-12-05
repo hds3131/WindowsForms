d4edf44032a36039993c935cebd8f51979f57518
Tarun Ramasamy Tamilselvan <tr519@student.aru.ac.uk>
2024-12-05 12:36:27 +0000

    linking

--- WindowsForms/Form10.Designer.cs
+++ WindowsForms/Form10.Designer.cs
@@ -59,9 +59,10 @@
             this.upcomingEventsLabel.AutoSize = true;
             this.upcomingEventsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.upcomingEventsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.upcomingEventsLabel.Location = new System.Drawing.Point(231, 109);
+            this.upcomingEventsLabel.Location = new System.Drawing.Point(346, 170);
+            this.upcomingEventsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.upcomingEventsLabel.Name = "upcomingEventsLabel";
-            this.upcomingEventsLabel.Size = new System.Drawing.Size(176, 24);
+            this.upcomingEventsLabel.Size = new System.Drawing.Size(284, 37);
             this.upcomingEventsLabel.TabIndex = 3;
             this.upcomingEventsLabel.Text = "Upcoming Events";
             // 
@@ -70,14 +71,15 @@
             this.upcomingEventsList.BackColor = System.Drawing.Color.Tomato;
             this.upcomingEventsList.Font = new System.Drawing.Font("Arial", 10F);
             this.upcomingEventsList.FormattingEnabled = true;
-            this.upcomingEventsList.ItemHeight = 19;
+            this.upcomingEventsList.ItemHeight = 32;
             this.upcomingEventsList.Items.AddRange(new object[] {
             "Event 1 - 12th Dec 2024",
             "Event 2 - 15th Dec 2024",
             "Event 3 - 20th Dec 2024"});
-            this.upcomingEventsList.Location = new System.Drawing.Point(235, 143);
+            this.upcomingEventsList.Location = new System.Drawing.Point(352, 223);
+            this.upcomingEventsList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.upcomingEventsList.Name = "upcomingEventsList";
-            this.upcomingEventsList.Size = new System.Drawing.Size(634, 99);
+            this.upcomingEventsList.Size = new System.Drawing.Size(949, 132);
             this.upcomingEventsList.TabIndex = 4;
             // 
             // quickActionsLabel
@@ -85,9 +87,10 @@
             this.quickActionsLabel.AutoSize = true;
             this.quickActionsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.quickActionsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.quickActionsLabel.Location = new System.Drawing.Point(231, 500);
+            this.quickActionsLabel.Location = new System.Drawing.Point(346, 781);
+            this.quickActionsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.quickActionsLabel.Name = "quickActionsLabel";
-            this.quickActionsLabel.Size = new System.Drawing.Size(141, 24);
+            this.quickActionsLabel.Size = new System.Drawing.Size(232, 37);
             this.quickActionsLabel.TabIndex = 5;
             this.quickActionsLabel.Text = "Quick Actions";
             this.quickActionsLabel.Click += new System.EventHandler(this.quickActionsLabel_Click);
@@ -97,9 +100,10 @@
             this.bookEventButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
             this.bookEventButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.bookEventButton.ForeColor = System.Drawing.Color.White;
-            this.bookEventButton.Location = new System.Drawing.Point(235, 537);
+            this.bookEventButton.Location = new System.Drawing.Point(352, 839);
+            this.bookEventButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.bookEventButton.Name = "bookEventButton";
-            this.bookEventButton.Size = new System.Drawing.Size(120, 40);
+            this.bookEventButton.Size = new System.Drawing.Size(180, 62);
             this.bookEventButton.TabIndex = 6;
             this.bookEventButton.Text = "Book Event";
             this.bookEventButton.UseVisualStyleBackColor = false;
@@ -110,9 +114,10 @@
             this.cancelBookingButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
             this.cancelBookingButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.cancelBookingButton.ForeColor = System.Drawing.Color.White;
-            this.cancelBookingButton.Location = new System.Drawing.Point(376, 542);
+            this.cancelBookingButton.Location = new System.Drawing.Point(564, 847);
+            this.cancelBookingButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cancelBookingButton.Name = "cancelBookingButton";
-            this.cancelBookingButton.Size = new System.Drawing.Size(150, 30);
+            this.cancelBookingButton.Size = new System.Drawing.Size(225, 47);
             this.cancelBookingButton.TabIndex = 7;
             this.cancelBookingButton.Text = "Cancel Booking";
             this.cancelBookingButton.UseVisualStyleBackColor = false;
@@ -121,7 +126,8 @@
             // eventCalendar
             // 
             this.eventCalendar.BackColor = System.Drawing.Color.Tomato;
-            this.eventCalendar.Location = new System.Drawing.Point(235, 283);
+            this.eventCalendar.Location = new System.Drawing.Point(352, 442);
+            this.eventCalendar.Margin = new System.Windows.Forms.Padding(14, 14, 14, 14);
             this.eventCalendar.MaxSelectionCount = 1;
             this.eventCalendar.Name = "eventCalendar";
             this.eventCalendar.TabIndex = 8;
@@ -136,19 +142,21 @@
             this.dataGridView2.BackgroundColor = System.Drawing.Color.Tomato;
             this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(1, 99);
+            this.dataGridView2.Location = new System.Drawing.Point(2, 155);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(169, 506);
+            this.dataGridView2.Size = new System.Drawing.Size(254, 791);
             this.dataGridView2.TabIndex = 10;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(27, 170);
+            this.pictureBox1.Location = new System.Drawing.Point(40, 266);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 109);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 170);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
@@ -158,9 +166,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.label1.Location = new System.Drawing.Point(231, 250);
+            this.label1.Location = new System.Drawing.Point(346, 391);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.Size = new System.Drawing.Size(250, 37);
             this.label1.TabIndex = 13;
             this.label1.Text = "Event Calendar";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -170,11 +179,12 @@
             this.dataGridView3.BackgroundColor = System.Drawing.Color.Tomato;
             this.dataGridView3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(1, -1);
+            this.dataGridView3.Location = new System.Drawing.Point(2, -2);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(179, 94);
+            this.dataGridView3.Size = new System.Drawing.Size(268, 147);
             this.dataGridView3.TabIndex = 18;
             // 
             // dataGridView1
@@ -182,42 +192,48 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.Tomato;
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(185, -1);
+            this.dataGridView1.Location = new System.Drawing.Point(278, -2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(747, 94);
+            this.dataGridView1.Size = new System.Drawing.Size(1120, 147);
             this.dataGridView1.TabIndex = 12;
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Tomato;
             this.button4.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(320, 9);
+            this.button4.Location = new System.Drawing.Point(480, 14);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(93, 65);
+            this.button4.Size = new System.Drawing.Size(140, 102);
             this.button4.TabIndex = 53;
             this.button4.Text = "Events";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Tomato;
             this.button3.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(431, 9);
+            this.button3.Location = new System.Drawing.Point(646, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(159, 65);
+            this.button3.Size = new System.Drawing.Size(238, 102);
             this.button3.TabIndex = 52;
             this.button3.Text = "Community and Chats";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Tomato;
             this.button2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(200, 9);
+            this.button2.Location = new System.Drawing.Point(300, 14);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 65);
+            this.button2.Size = new System.Drawing.Size(150, 102);
             this.button2.TabIndex = 51;
             this.button2.Text = "Dashboard";
             this.button2.UseVisualStyleBackColor = false;
@@ -228,23 +244,26 @@
             this.dataGridView4.BackgroundColor = System.Drawing.Color.Tomato;
             this.dataGridView4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView4.Location = new System.Drawing.Point(191, -1);
+            this.dataGridView4.Location = new System.Drawing.Point(286, -2);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.RowHeadersWidth = 51;
             this.dataGridView4.RowTemplate.Height = 24;
-            this.dataGridView4.Size = new System.Drawing.Size(720, 94);
+            this.dataGridView4.Size = new System.Drawing.Size(1080, 147);
             this.dataGridView4.TabIndex = 50;
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.Tomato;
             this.button5.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(606, 9);
+            this.button5.Location = new System.Drawing.Point(909, 14);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(177, 65);
+            this.button5.Size = new System.Drawing.Size(266, 102);
             this.button5.TabIndex = 54;
             this.button5.Text = "FeedBack";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // linkLabel1
             // 
@@ -253,30 +272,33 @@
             this.linkLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.Black;
-            this.linkLabel1.Location = new System.Drawing.Point(47, 353);
+            this.linkLabel1.Location = new System.Drawing.Point(1206, 43);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(80, 28);
+            this.linkLabel1.Size = new System.Drawing.Size(125, 44);
             this.linkLabel1.TabIndex = 55;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Logout";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Tomato;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Location = new System.Drawing.Point(18, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 23);
+            this.textBox1.Size = new System.Drawing.Size(237, 37);
             this.textBox1.TabIndex = 56;
             this.textBox1.Text = "Togather Culture";
             // 
             // Form10
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSalmon;
-            this.ClientSize = new System.Drawing.Size(920, 600);
+            this.ClientSize = new System.Drawing.Size(1380, 938);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button5);
@@ -295,6 +317,7 @@
             this.Controls.Add(this.quickActionsLabel);
             this.Controls.Add(this.upcomingEventsList);
             this.Controls.Add(this.upcomingEventsLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form10";
             this.Text = "Together Culture - Community Events";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
