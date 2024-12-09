603f48308d8db019608c91d4f68b406d957803f8
Tarun Ramasamy Tamilselvan <tr519@student.aru.ac.uk>
2024-12-09 17:24:45 +0000

    Create SQLQuery1.sql

---
+++ WindowsForms/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE BillingDetails (
+    ID INT IDENTITY(1,1) PRIMARY KEY,  -- Auto-incrementing ID
+    FirstName NVARCHAR(50) NOT NULL,   -- Billing first name
+    LastName NVARCHAR(50) NOT NULL,    -- Billing last name
+    Address NVARCHAR(255) NOT NULL     -- Billing address
+);
