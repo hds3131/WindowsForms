c1365e05a3cd5ead4e24fcedca7c82133c59450a
James Wilson <JW1515@student.aru.ac.uk>
2024-12-11 15:31:56 +0000

    database changes

--- WindowsForms/Form10.cs
+++ WindowsForms/Form10.cs
@@ -18,7 +18,8 @@ namespace WindowsForms
 
         private void LoadUpcomingEvents()
         {
-            string connectionString = @"Server=np:\\.\pipe\LOCALDB#653b9183\tsql\query;Database=mydbs;Integrated Security=true;";
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ServiceBasedDB"].ConnectionString;
+
             string query = "SELECT EventName, EventDate, Location, Attendees, EventDetails, EventType FROM events ORDER BY EventDate";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
